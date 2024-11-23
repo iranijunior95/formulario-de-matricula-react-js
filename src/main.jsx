2290295b4edea4c49d7df417156f2ec66fb0f89f
Irani junior <iranijunior95@gmail.com>
2024-11-23 10:52:04 -0300

    commit: configuração das rotas / criação layout pagina home

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
 import Home from './pages/Home';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,26 @@
+import { NavLink } from 'react-router';
+import Frame01 from '../../assets/img/frame-home-01.svg';
+import Frame02 from '../../assets/img/frame-home-02.svg';
 import './style.css';
 
 function Home() {
     return (
         <div className='home'>
-            home
+            <section className='home-header'>
+                <div className='header-cabecalho'>
+                    <img src={Frame01} alt="frame-01" />
+                    <span>Estrelas do Amanhã</span>
+                </div>
+                
+                <h1>Porque cada momento de <span>aprendizado</span> conta</h1>
+                <h2>Inscreva seu filho em nossa escola e veja-o florescer em um ambiente acolhedor, seguro e estimulante.</h2>
+            </section>
+
+            <picture className='home-main-img'>
+                <img src={Frame02} alt="frame-02" />
+            </picture>
+
+            <NavLink to="/formulario" className="btn-default">Acessar Formulário</NavLink>
         </div>
     );
 }
